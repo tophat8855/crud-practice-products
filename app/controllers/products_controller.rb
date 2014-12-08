2f100d79a08a55a79bd0dd734a61ac1ab24cb478
tophat8855 <hrfarley@gmail.com>
2014-12-08 10:51:40 -0800

    User can edit product using edit link in show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path @product
+    else
+      render :edit
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
